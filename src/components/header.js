2e5507ddf9dec3029de6884040ffd81849717523
Eklipxe <93890931+Eklipxe@users.noreply.github.com>
2022-06-17 06:20:28 -0500

    Header complete

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header className="header">
+            <nav className="nav">
+            <i className="nav--logo fa-solid fa-earth-africa"></i>
+            <h1 className="nav--title" >My Travel Journal</h1>
+            </nav>
+        </header>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: 'Mount Kilimanjaro',
+        location: "Tanzania",
+        googleMapsUrl: 'https://goo.gl/maps/1P3qgtt1hCjzL9uc7',
+        startDate: "4 Mar, 2022",
+        endDate: "19 Mar, 2022",
+        description: "Kilimanjaro is the fourth most topographically prominent peak on Earth. It is part of Kilimanjaro National Park and is a major hiking and climbing destination.",
+        imageUrl: 'https://images.unsplash.com/photo-1518729571365-9a891a9df2bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+
+    },
+]
